124905899abf7a7793a64ad8f0383bd5b80860f1
rajesh-rahul <rahul-rajesh@outlook.com>
2022-12-05 22:56:13 -0800

    2022 Day 6 Improved

---
+++ improved/day6/src/main.rs
@@ -0,0 +1,27 @@
+use std::collections::HashSet;
+
+fn main() {
+    let input = std::fs::read_to_string("inputs/day6/day6.txt").unwrap();
+
+    // Puzzle 1
+    let size = 4;
+    for (idx, slice) in input.as_bytes().windows(size).enumerate() {
+        let set: HashSet<_> = slice.iter().map(|&ch| ch as char).collect();
+        
+        if set.len() == size {
+            println!("Puzzle 2: {}", idx + size);
+            break;
+        }
+    }
+    
+    // Puzzle 2
+    let size = 14;
+    for (idx, slice) in input.as_bytes().windows(size).enumerate() {
+        let set: HashSet<_> = slice.iter().map(|&ch| ch as char).collect();
+
+        if set.len() == size {
+            println!("Puzzle 2: {}", idx + size);
+            break;
+        }
+    }
+}
